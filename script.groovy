fc38b93b67d36d63d285b2c2afd020ec879e547b
Marcin Lament <marcin@clearscore.com>
2018-05-11 08:57:50 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -86,7 +86,7 @@ def hasApprovedWithoutQALabel(Issue issue) {
 	}
 	
 	if (approved && issue.requestedReviewers.size() == 0 && !issue.labels.contains("QA")) {
-		String user = users[issue.author]
+		String user = (users[issue.author] != null) ? users[issue.author] : issue.author
 		return formatMessage(issue, user)
 	}
 	return ""
